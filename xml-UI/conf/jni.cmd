43066f9c804bae44f9aa29b43fa0ea31da9420e1
Administrator <Administrator@10.117.61.136>
2016-02-18 12:50:15 +0800

    tidy up project structure
    1.remove project root folders

--- xml-UI/conf/jni.cmd
+++
@@ -1,7 +0,0 @@
-@rem java native method compile
-@SET JAVA_HOME=E:\software\Java\jdk1.8.0_40
-@rem %JAVA_HOME%\bin\javah.exe -d ..\src\main\cpp -verbose -classpath ..\target\classes king.flow.control.driver.ICCardConductor
-@rem %JAVA_HOME%\bin\javah.exe -d ..\src\main\cpp -verbose -classpath ..\target\classes king.flow.control.driver.KeyBoardDriver
-@rem %JAVA_HOME%\bin\javah.exe -d ..\src\main\cpp -verbose -classpath ..\target\classes king.flow.control.driver.MagnetCardConductor
-%JAVA_HOME%\bin\javah.exe -d ..\src\main\cpp -verbose -classpath ..\target\classes king.flow.control.driver.TwoInOneCardConductor
-@rem %JAVA_HOME%\bin\javah.exe -d ..\src\main\cpp -verbose -classpath ..\target\classes king.flow.control.driver.PrinterConductor
