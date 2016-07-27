57686cf3bd5cc242ae9320c6e22ec6bec80710b7
liujin <liujin@rongkehuitong.com>
2016-07-27 14:52:17 +0800

    give agent startup prompt
    1.when agent starts up, a dialog will show some text information

--- xml-UI/src/main/java/king/flow/control/agent/AppAgent.java
+++ xml-UI/src/main/java/king/flow/control/agent/AppAgent.java
@@ -5,7 +5,15 @@
  */
 package king.flow.control.agent;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import static king.flow.control.BankAppStarter.DEFAULT_LOG_CONF;
 import static king.flow.control.BankAppStarter.LOG_CONF;
 import king.flow.net.TunnelBuilder;
@@ -25,8 +33,21 @@ public class AppAgent {
         TunnelBuilder.getTunnelBuilder().enableHeartBeat();
         java.awt.EventQueue.invokeLater(() -> {
             JDialog jDialog = new JDialog();
+            jDialog.setLayout(new BorderLayout());
+            final JLabel jLabel = new JLabel("Succeed in starting bank agent");
+            jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            jLabel.setFont(new Font(Font.DIALOG, Font.BOLD, 21));
+            jLabel.setPreferredSize(new Dimension(400, 90));
+            jDialog.add(jLabel, BorderLayout.CENTER);
+            jDialog.setLocationRelativeTo(null);
+            jDialog.pack();
             jDialog.setVisible(true);
-            jDialog.setVisible(false);
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    jDialog.setVisible(false);
+                }
+            }, TimeUnit.SECONDS.toMillis(5));
         });
     }
 }
